8b7d68bcc7a10e7fb085c2f3cb7d4d7f316bab7b
alvinjaymaique <alvinjay.maique.56@gmail.com>
2025-01-21 21:42:21 +0800

    Add test case for QuestionIndexViewTests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,6 +1,7 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 
@@ -26,3 +27,86 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
 
         self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    """
+    Create a question with the given 'question_text' and published the given
+    number of `days` offset to now (negative for questions published in the
+    past, positive for questions that have yet to published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the index page.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_question_list"], [question],)
+
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't
+        displayed on the index page.
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        are displayed.
+        """
+        question = create_question(question_text="Past question", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_question_list"], [question],)
+
+    def test_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_question_list"], [question2, question1],)
+
+
+""" This is for shell using 'python manage.py shell' """
+# >>> from django.test import Client
+# >>> # create an instance of the client for our use
+# >>> client = Client()
+
+# >>> # get a response from '/'
+# >>> response = client.get("/")
+# Not Found: /
+# >>> # we should expect a 404 from that address; if you instead see an
+# >>> # "Invalid HTTP_HOST header" error and a 400 response, you probably
+# >>> # omitted the setup_test_environment() call described earlier.
+# >>> response.status_code
+# 404
+# >>> # on the other hand we should expect to find something at '/polls/'
+# >>> # we'll use 'reverse()' rather than a hardcoded URL
+# >>> from django.urls import reverse
+# >>> response = client.get(reverse("polls:index"))
+# >>> response.status_code
+# 200
+# >>> response.content
+# b'\n    <ul>\n    \n        <li><a href="/polls/1/">What&#x27;s up?</a></li>\n    \n    </ul>\n\n'
+# >>> response.context["latest_question_list"]
+# <QuerySet [<Question: What's up?>]>
